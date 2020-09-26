235809f0c658c5db5e44bce77c7e47c7081480c9
Rejwana Haque <rejwanahaque@Rejwanas-MacBook-Air.local>
2020-09-25 21:33:15 -0600

    Inintial Commit

---
+++ experiments/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+/Py/Engine_test_2.py
+
+/Py/engine_test.py
+
+/Py/best_move_accoracy.py 
---
+++ experiments/run_psntest.sh
@@ -0,0 +1,2 @@
+chmod 777 /Py/best_move_accoracy.py 
+python3 /Py/best_move_accoracy.py
